216c284eb4f13975c6ea52943e2e3fbccc41f3f1
Swetabh333 <swetabh333@gmail.com>
2023-10-14 23:08:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import  Wordcounter  from './components/Wordcounter';
+
+import './App.css'
+
+function App() {
+        return(
+        <Wordcounter></Wordcounter>
+        )
+ }
+
+export default App
